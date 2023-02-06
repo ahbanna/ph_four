3e2fe0b8b226ac450f8895213dd83f01d87572b2
Al Hasanul Banna <tutul5800@gmail.com>
2023-02-07 02:09:29 +0600

    second problem solved

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-function mindGame(number){
-    let threeMul = number * 3;
-    let addTen = threeMul + 10;
-    let divThree = addTen / 2;
-    let finalOutput = divThree - 5;
-    return finalOutput;
+function evenOdd(text){
+if(typeof text == "number" || text.length === 0){
+    return "Please enter text";
+    } else if(text.length % 2 === 0){
+        let evenText = "even";
+        return evenText;
+    }
+    else{
+        let oddText = "odd";
+        return oddText;
 }
-let number = 33;
-let result = mindGame(number);
+}
+let text = "Bangladesh";
+let result = evenOdd(text);
 console.log(result);
--- ph_four.js
+++ ph_four.js
@@ -1,4 +1,6 @@
-function mindGame(number){
+// Problem One 
+
+function evenOdd(number){
     let threeMul = number * 3;
     let addTen = threeMul + 10;
     let divThree = addTen / 2;
@@ -6,5 +8,24 @@ function mindGame(number){
     return finalOutput;
 }
 let number = 33;
-let result = mindGame(number);
+let result = evenOdd(number);
 console.log(result);
+
+
+// Problem Two
+function evenOdd(text){
+    if(typeof text == "number" || text.length === 0){
+        return "Please enter text";
+        } else if(text.length % 2 === 0){
+            let evenText = "even";
+            return evenText;
+        }
+        else{
+            let oddText = "odd";
+            return oddText;
+    }
+    }
+    let text = "Bangladesh";
+    let result = evenOdd(text);
+    console.log(result);
+    
